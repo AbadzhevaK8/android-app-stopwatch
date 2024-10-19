d86df1c20c8f721058ae6c9dff91e31f76ea8f74
k8abadzheva <make.me.loco@gmail.com>
2024-10-19 03:59:18 +0300

    Add ViewBindingPropertyDelegate
    Added ViewBindingPropertyDelegate library to simplify View Binding usage.
    
    - Enabled View Binding in the app module.
    - Added the ViewBindingPropertyDelegate-noreflection dependency.
    - Updated Proguard rules.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
@@ -45,4 +48,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.viewbindingpropertydelegate.noreflection)
 }
