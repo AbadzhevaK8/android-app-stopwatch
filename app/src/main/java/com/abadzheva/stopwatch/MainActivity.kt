7aaf3629310f2b8d9519a5d291c49bf7fef24a5b
k8abadzheva <make.me.loco@gmail.com>
2024-10-19 03:59:32 +0300

    feat: Implement basic stopwatch functionality
    Implemented the core functionality of the stopwatch:
    
    - Added UI elements: Chronometer for display, buttons for Start, Pause, and Reset.
    - Implemented start, pause, and reset logic.
    - Used ViewBinding for UI interaction.
    - Updated layout to a LinearLayout for better organization.
    - Added string resources for button labels.

--- app/src/main/java/com/abadzheva/stopwatch/MainActivity.kt
+++ app/src/main/java/com/abadzheva/stopwatch/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.abadzheva.stopwatch
 
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.abadzheva.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::bind, R.id.main)
+    private var running = false
+    private var offset: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.startButton.setOnClickListener {
+            if (!running) {
+                setBaseTime()
+                binding.stopwatch.start()
+                running = true
+            }
+        }
+
+        binding.pauseButton.setOnClickListener {
+            if (running) {
+                saveOffset()
+                binding.stopwatch.stop()
+                running = false
+            }
+        }
+
+        binding.resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
+        }
+    }
+
+    private fun saveOffset() {
+        binding.stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
+    private fun setBaseTime() {
+        offset = SystemClock.elapsedRealtime() - binding.stopwatch.base
     }
 }
